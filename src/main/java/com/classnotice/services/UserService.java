3ad8d815ed9768b8fc7fd31f2c4d10def3e8ef86
Zumium <martin007323@gmail.com>
2016-05-29 22:04:48 +0800

    unfinished user login vertification

--- src/main/java/com/classnotice/services/UserService.java
+++ src/main/java/com/classnotice/services/UserService.java
@@ -1,14 +1,43 @@
 package com.classnotice.services;
 
 import com.classnotice.beans.Account;
+import com.classnotice.db.entities.Student;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 @Service
 public class UserService {
+
+	@Autowired
+	@Qualifier("dataSource")
+	private DataSource dataSource;
+
 	public boolean vertifyUserAccount(Account account) {
-		String user=(String)account.getId();
+		Connection conn=null;
+		boolean result=false;
+		try{
+			conn=dataSource.getConnection();
+			Student loginStudent=Student.Query(conn,account.getId());
+			if(loginStudent==null){
+				result=false;
+			}
+			result=loginStudent.getPassword().equals(account.getPassword());
+		}
+		catch(Exception e){
+			result=false;
+		}
+		finally{
+			conn.close();
+		}
+
+		return result;
+		/*String user=(String)account.getId();
 		String password=(String)account.getPassword();
-		return (user.equals("2014220402028")&&password.equals("123456"));
+		return (user.equals("2014220402028")&&password.equals("123456")); */
 	}
 }
