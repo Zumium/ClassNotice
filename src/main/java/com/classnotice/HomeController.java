c6d99feeb28af49ea79df35b262f1d34ac35619b
Zumium <martin007323@gmail.com>
2016-05-29 12:13:05 +0800

    initiate commit

---
+++ src/main/java/com/classnotice/HomeController.java
@@ -0,0 +1,16 @@
+package com.classnotice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.ModelMap;
+
+@Controller
+public class HomeController{
+
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String printHello(ModelMap model){
+		model.addAttribute("message","Hello,Spring MVC");
+		return "hello";
+	}
+}
