13624b9367d487004e267207296940dc3998e395
Zumium <martin007323@gmail.com>
2016-05-31 15:38:20 +0800

    add login controller test

---
+++ src/test/java/LoginControllerTest.java
@@ -0,0 +1,48 @@
+import com.classnotice.services.UserService;
+import com.classnotice.controllers.LoginController;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("file:src/main/webapp/WEB-INF/mainservlet.xml")
+public class LoginControllerTest {
+	private MockMvc mockMvc;
+
+	@Mock
+	private UserService userService;
+
+	@InjectMocks
+	private LoginController loginController;
+
+	@Before
+	public void setup(){
+		MockitoAnnotations.initMocks(this);
+		this.mockMvc=MockMvcBuilders.standaloneSetup(loginController).build();
+	}
+
+	@Test
+	public void shouldLoginSuccessfully() throws Exception{
+		mockMvc.perform(post("/login")
+			.param("id", "2014220402028")
+			.param("password", "123456"))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
+}
