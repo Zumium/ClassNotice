4d80e04e4763105727c35a0a20e0f7ae2a5b121f
Zumium <martin007323@gmail.com>
2016-07-10 20:42:09 +0800

    add 404 error handing code

--- src/main/java/com/classnotice/controllers/ExceptionHandlingController.java
+++ src/main/java/com/classnotice/controllers/ExceptionHandlingController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.classnotice.exceptions.NoPermissionException;
 
@@ -14,5 +15,11 @@ public class ExceptionHandlingController{
 	@ExceptionHandler(NoPermissionException.class)
 	public String handleForbiddenException(){
 		return "forbiddenError";
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(EmptyResultDataAccessException.class)
+	public String handleNotFoundException(){
+		return "notFoundError";
 	}
 }
---
+++ src/main/webapp/WEB-INF/jsp/notFoundError.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <title>404</title>
+    <body>
+        <h2>404 NOT FOUND</h2>
+	<p>您请求的页面不存在</p>
+    </body>
+</html>
